6e27001394fb05f4e8706bc554753d7101846c72
ArcherHeffern <heffernarcher@gmail.com>
2023-05-05 17:32:45 -0400

    updated gpt model to work

--- routes/archer.js
+++ routes/archer.js
@@ -6,13 +6,8 @@
   which is a free service provided to the world.
 */
 const express = require('express');
-const { Configuration, OpenAIApi } = require("openai");
-if (!process.env.OPENAI_API_KEY) {throw new Error('OPENAI_API_KEY is required')}
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
 const router = express.Router();
+const axios = require('axios');
 
 isLoggedIn = (req,res,next) => {
   if (res.locals.loggedIn) {
@@ -28,19 +23,10 @@ router.get('/archer', isLoggedIn, (req,res) => {
 
 router.post('/api/v1/prompt/archer', isLoggedIn, async (req,res) => {
   if (!req.body.prompt) {res.status(400).json({error: 'prompt is required'}).send(); return}
-  console.log(req.body.prompt)
-  try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: req.body.prompt,
-    });
-    res.status(200).send(completion.data.choices[0].text);
-    return;
-  } catch (error) {
-    res.status(500).json({error});
-    return
-    }
-  }
-)
+  const response = await axios.post('http://gracehopper.cs-i.brandeis.edu:3500/openai',
+  {prompt:req.body.prompt})
+  res.status(200).send(response.data.choices[0]['message']['content']);
+  return;
+  })
 
   module.exports = router;
